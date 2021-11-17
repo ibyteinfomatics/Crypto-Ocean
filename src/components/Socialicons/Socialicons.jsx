6a916bd96213f6e93431b7466f642486b35b8ca4
jatin dang <djatin88@gmail.com>
2021-11-17 23:45:27 +0530

    Updated youtube icon and wave lines issue resolved

--- src/components/Socialicons/Socialicons.jsx
+++ src/components/Socialicons/Socialicons.jsx
@@ -3,6 +3,7 @@ const discord = "../assets/images/discord.png";
 const insta = "../assets/images/insta-link.png";
 const twtr = "../assets/images/twtr-link.png";
 const newIcon = "../assets/images/new-icon.png";
+const ytIcon = "../assets/images/yt.png";
 
 function Socialicons(){
     return(
@@ -20,6 +21,11 @@ function Socialicons(){
             <li>
                 <a href="https://twitter.com/cryptoceanart" target="_blank">
                     <img src={twtr} alt="Twitter" />
+                </a>
+            </li>
+            <li>
+                <a href="javascript:void(0)">
+                    <img src={ytIcon} alt="Youtube Icon" />
                 </a>
             </li>
             <li>
